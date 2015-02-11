3b8a35202e1dd3f547911ed2e25974bed28ce845
wikidsystems <nowen@wikidsystems.com>
2015-02-11 17:09:50 -0500

    minor updates

--- openvpn_wikid/provisioners/openvpn.sh
+++ openvpn_wikid/provisioners/openvpn.sh
@@ -1,5 +1,4 @@
 sudo yum -y upgrade ca-certificates --disablerepo=epel
-#yum -y update
 cp /root/files/pam_radius_auth.so /usr/lib64/security/
 cp /root/files/pam_radius_auth.so /lib64/security/
 mkdir /etc/raddb/
@@ -30,26 +29,16 @@ cp /root/files/server.conf /etc/openvpn
 
 
 mkdir -p /etc/openvpn/easy-rsa/keys
-
 cp /usr/share/easy-rsa/2.0/* /etc/openvpn/easy-rsa/
-
 cp /root/files/vars /etc/openvpn/easy-rsa/
-
-
 cd /etc/openvpn/easy-rsa/
-
 source ./vars
 
 ./clean-all
-
 ./build-ca --batch
-
 ./build-key-server --batch server
-
 ./build-dh 
-
 ./build-key --batch client
-
 ./build-key --batch client2
 
 cp /etc/openvpn/easy-rsa/keys/dh2048.pem /etc/openvpn
@@ -72,39 +61,4 @@ service openvpn start
 chkconfig openvpn on
 
 
-
-cd /etc/openvpn/easy-rsa/
-
-source ./vars
-
-./clean-all
-
-./build-ca --batch
-
-./build-key-server --batch server
-
-./build-dh 
-
-./build-key --batch client
-
-./build-key --batch client2
-
-cp /etc/openvpn/easy-rsa/keys/dh2048.pem /etc/openvpn
-rm /etc/openvpn/ca.crt
-cp /etc/openvpn/easy-rsa/keys/ca.crt /etc/openvpn
-rm /etc/openvpn/server.crt
-cp /etc/openvpn/easy-rsa/keys/server.crt /etc/openvpn
-rm /etc/openvpn/server.key
-cp /etc/openvpn/easy-rsa/keys/server.key /etc/openvpn
-
-# commented for centos 6 on amazon
-#firewall-cmd --add-service openvpn
-#firewall-cmd --permanent --add-service openvpn
-#firewall-cmd --add-masquerade
-#firewall-cmd --permanent --add-masquerade
-#systemctl start openvpn@server.service
-#systemctl enable openvpn@server.service
-
-service openvpn start
-chkconfig openvpn on
 

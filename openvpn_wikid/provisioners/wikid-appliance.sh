4b86b9a870fd1d3663744c2cc7b351afbda44d1c
wikidsystems <nowen@wikidsystems.com>
2016-06-15 18:23:17 -0400

    remove rpm, update provisioner

--- openvpn_wikid/provisioners/wikid-appliance.sh
+++ openvpn_wikid/provisioners/wikid-appliance.sh
@@ -3,7 +3,7 @@ mv -f /root/files/motd /etc/
 mkdir /root/.ssh
 #cp /root/files/authorized_keys /root/.ssh/authorized_keys
 rm /root/.ssh/authorized_keys
-sudo yum -y update
+#sudo yum -y update
 sudo yum install -y java-1.6.0-openjdk postgresql postgresql-libs postgresql-jdbc postgresql-server postgresql-pl compat-libstdc++-296 ntp system-config-date perl-libwww-perl mlocate vim rsync
 service postgresql initdb
 mkdir -p /opt/WiKID/conf
